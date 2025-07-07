6b45590cfb90e05aacd4d32014e60b14390b31ae
AmberWein <amberw.dev@gmail.com>
2025-07-07 13:44:58 +0300

    ex1- Determine whether the following are truthy or falsey

---
+++ js-and-algo/JavaScript-Basics/ex-1.js
@@ -0,0 +1,28 @@
+console.log('Determine whether the following are truthy or falsey:');
+
+console.log('1. (5 > 2) && false');
+console.log((5 > 2) && false);
+
+console.log('2. !("knife" === "sword")');
+console.log(!("knife" === "sword"));
+
+console.log('3. (1 < 2) || (-1 > -1) || !false');
+console.log((1 < 2) || (-1 > -1) || !false);
+
+console.log('4. ""');
+console.log(Boolean(""));
+
+console.log('5. (31 % 5) == "1"');
+console.log((31 % 5) == "1");
+
+console.log('6. !!true');
+console.log(!!true);
+
+console.log('7. "5th Avenue" != "5th Avenue"');
+console.log("5th Avenue" != "5th Avenue");
+
+console.log('8. 52 !== "52"');
+console.log(52 !== "52");
+
+console.log('9. (undefined || null)');
+console.log(undefined || null);
