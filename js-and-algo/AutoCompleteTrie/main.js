f8db1c3da8619e8a9743ac69f349648747013346
AmberWein <amberw.dev@gmail.com>
2025-07-21 12:52:07 +0300

    add an 'add' function

--- js-and-algo/AutoCompleteTrie/main.js
+++ js-and-algo/AutoCompleteTrie/main.js
@@ -4,12 +4,29 @@ const AutoCompleteTrie = require("./AutoCompleteTrie");
 const trie = new AutoCompleteTrie();
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "> "
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
 });
 
 console.log("=== AutoComplete Trie Console ===");
 console.log("Type 'help' for commands\n");
 
 rl.prompt();
+
+rl.on("line", (line) => {
+  const input = line.trim();
+  const [command, ...args] = input.split(" ");
+
+  switch (command) {
+    case "add":
+      if (args.length === 0) {
+        console.log("✗ Please provide a word to add");
+      } else {
+        const word = args[0];
+        trie.addWord(word);
+        console.log(`✓ Added '${word}' to dictionary`);
+      }
+      break;
+  }
+});
