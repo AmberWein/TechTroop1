a6be41c4648977e34189303ebe8a36f3c155ca4a
AmberWein <amberw.dev@gmail.com>
2025-07-31 13:12:33 +0300

    se try/catch

--- js-and-web/Async-Await/ex-1.js
+++ js-and-web/Async-Await/ex-1.js
@@ -1,5 +1,6 @@
 // Given Promise-based code:
 async function getUserById(userId) {
+  try {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
 
     if (!response.ok) {
@@ -9,4 +10,8 @@ async function getUserById(userId) {
     const user = await response.json();
     console.log(`Found user: ${user.name} (${user.email})`);
     return user;
+  } catch (error) {
+    console.error('Error fetching user:', error.message);
+    return null;
+  }
 }
