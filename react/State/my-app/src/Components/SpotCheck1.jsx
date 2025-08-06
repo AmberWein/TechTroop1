c7779b7c648d75cc86abe3a184dc48421d160df5
AmberWein <amberw.dev@gmail.com>
2025-08-06 14:01:37 +0300

    finish state spot check 1

---
+++ react/State/my-app/src/Components/SpotCheck1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SpotCheck1 = () => {
+
+  const logHover = () => {
+    console.log("I was hovered!")
+  }
+
+  return (
+    <button onMouseEnter={logHover}>Click me</button>
+  );
+}
+
+export default SpotCheck1
