7503b2c68b9a279b10df745733d2de157bd156de
AmberWein <amberw.dev@gmail.com>
2025-08-10 11:43:25 +0300

    Exercise2: create a basic "conversation" app

--- react/Data-Flow/src/App.jsx
+++ react/Data-Flow/src/App.jsx
@@ -1,11 +1,13 @@
 import Exercise1 from './Components/Exercise1/Exercise1'
+import Exercise2 from './Components/Exercise2/Exercise2'
 import './App.css'
 
 function App() {
 
   return (
     <div className='main-container'>
-      <Exercise1/>
+      <div>Exercise 1<Exercise1/></div>
+      <div>Exercise 2<Exercise2/></div>
     </div>
   )
 }
