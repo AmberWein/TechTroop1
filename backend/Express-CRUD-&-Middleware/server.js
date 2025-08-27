b67f1a75effe6e58b4c2639cf973835504d8b85c
AmberWein <amberw.dev@gmail.com>
2025-08-27 17:07:21 +0300

    Exercise 2: create a GET route to check a word’s count

--- backend/Express-CRUD-&-Middleware/server.js
+++ backend/Express-CRUD-&-Middleware/server.js
@@ -9,3 +9,11 @@ app.get("/sanity", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
+const wordCounter = {};  // track word counts
+
+app.get("/word/:word", (req, res) => {
+  const word = req.params.word.toLowerCase();
+  const count = wordCounter[word] || 0;
+  res.send({ count });
+});
