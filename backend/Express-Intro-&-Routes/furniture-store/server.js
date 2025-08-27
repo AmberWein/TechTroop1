e7b7331f514896fbb24afdd867d5039d916f1853
AmberWein <amberw.dev@gmail.com>
2025-08-27 15:22:06 +0300

    Exercise 2: create a /priceCheck/:name route that returns the price of a furniture item

--- backend/Express-Intro-&-Routes/furniture-store/server.js
+++ backend/Express-Intro-&-Routes/furniture-store/server.js
@@ -2,8 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const store = [
+    { name: "table", inventory: 3, price: 800 },
+    { name: "chair", inventory: 16, price: 120 },
+    { name: "couch", inventory: 1, price: 1200 },
+    { name: "picture frame", inventory: 31, price: 70 }
+]
+
 app.get("/", (req, res) => {
-  res.send("Server is up and running.");
+  res.send("Server is up and running smoothly");
+});
+
+app.get("/priceCheck/:name", (req, res) => {
+  const itemName = req.params.name.toLowerCase();
+  const item = store.find(product => product.name === itemName);
+
+  if (item) {
+    res.send({ price: item.price });
+  } else {
+    res.send({ price: null });
+  }
 });
 
 app.listen(PORT, () => {
